e46bc9110069f8908e8b238701f10ef4ffd64ed7
Niels Stockfleth <nyls@aol.com>
2021-01-02 23:23:19 +0100

    Hamburger menu for Downloads page

--- APIServer/wwwroot/js/downloads/downloads.js
+++ APIServer/wwwroot/js/downloads/downloads.js
@@ -2,17 +2,37 @@
 
 var downloads = (() => {
 
+    const renderDropdown = (parent) => {
+        let dropdownDiv = controls.create(parent, "div", "dropdown");
+        let dropdownButton = controls.createImg(dropdownDiv, "dropbtn", 24, 24, "/images/downloads/hamburger.svg");
+        dropdownButton.addEventListener("click", () => {
+            document.getElementById("dropdown-id").classList.toggle("show");
+        });
+        let dropdownContentDiv = controls.create(dropdownDiv, "div", "dropdown-content");
+        dropdownContentDiv.id = "dropdown-id";
+    };
+
+    const renderDropdownContent = () => {
+        let parent = document.getElementById("dropdown-id");
+        if (!parent) return;
+        controls.removeAllChildren(parent);
+        controls.createA(parent, undefined, "/slideshow", "Bildergalerie");
+        controls.createA(parent, undefined, "/notes", "Notizen");
+        controls.createA(parent, undefined, "/skat", "Skat");
+        controls.createA(parent, undefined, "/diary", "Tagebuch");
+        controls.createA(parent, undefined, "/tetris", "Tetris");
+    };
+
     const renderCopyright = (parent) => {
         let div = controls.createDiv(parent);
-        controls.create(div, "span", "copyright", `Myna Downloads. Copyright 2020 `);
+        controls.create(div, "span", "copyright", "Myna Downloads. Copyright 2020-2021 ");
         let a = controls.createA(div, "copyright", "https://github.com/nylssoft/", "Niels Stockfleth");
         a.target = "_blank";
-        let time = new Date().toLocaleTimeString("de-DE");
-        controls.create(div, "span", "copyright", `. Alle Rechte vorbehalten. Letzte Aktualisierung: ${time}. `);
-        controls.createA(div, "copyright", "/slideshow", "Home");
+        controls.create(div, "span", "copyright", ".");
     };
 
     const renderMainPage = (parent, apps) => {
+        renderDropdown(parent);
         controls.create(parent, "h1", undefined, "Downloads f\u00FCr Windows 10");
         apps.forEach( (app) => {
             let divApp = controls.createDiv(parent, "app");
@@ -28,13 +48,13 @@ var downloads = (() => {
                     ap.target = "_blank";
                 }
             }
-            controls.createImg(divApp, undefined, 400, 200, app.image);
+            controls.createImg(divApp, undefined, 350, 200, app.image);
         });
         renderCopyright(controls.createDiv(parent));
+        renderDropdownContent();
     };
 
     const render = (json) => {
-        console.log(json);
         controls.removeAllChildren(document.body);
         let divMain = controls.createDiv(document.body);
         renderMainPage(divMain, json.apps);
