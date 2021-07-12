4d4ec31db7ab19f832d75de3bc27d962f4744850
Niels Stockfleth <nyls@aol.com>
2021-07-12 07:45:47 +0200

    Slideshow fix to hide controls if picture is clicked

--- APIServer/wwwroot/js/common/utils.js
+++ APIServer/wwwroot/js/common/utils.js
@@ -320,6 +320,7 @@ var utils = (() => {
             parent = document.body;
         }        
         const dropdownDiv = controls.create(parent, "div", "dropdown");
+        dropdownDiv.id = "div-dropdown-id";
         const dropdownButton = controls.createImg(dropdownDiv, "dropbtn", 24, 24, "/images/buttons/hamburger.svg");
         const dropdownContentDiv = controls.create(dropdownDiv, "div", "dropdown-content");
         dropdownContentDiv.id = "dropdown-id";
