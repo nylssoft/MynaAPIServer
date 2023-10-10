8d7b4dc191e16748bb4926def68ab1b4a439ab61
Niels Stockfleth <nyls@aol.com>
2023-10-10 06:42:26 +0200

    Cleanup Skat database source files

--- APIServer/Database/DbSkatGameHistory.cs
+++ APIServer/Database/DbSkatGameHistory.cs
@@ -1,9 +1,21 @@
-﻿using SQLitePCL;
-using System;
-using System.Collections.Generic;
+﻿/*
+    Myna API Server
+    Copyright (C) 2020-2023 Niels Stockfleth
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace APIServer.Database
 {
--- APIServer/Database/DbSkatReservation.cs
+++ APIServer/Database/DbSkatReservation.cs
@@ -16,7 +16,6 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APIServer.Database
